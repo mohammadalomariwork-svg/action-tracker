9f66904a58a2ad25302f4cb87916eab90c604dbd
mohammadalomariwork-svg <Mohammad.alomari.work@gmail.com>
2026-03-08 22:29:25 +0400

    202603082229-after escalation update

---
+++ backend/ActionTracker/ActionTracker.Infrastructure/Data/Migrations/20260308182034_ActionItemsEscalations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActionTracker.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActionItemsEscalations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActionItemEscalations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ActionItemId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Explanation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EscalatedByUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActionItemEscalations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ActionItemEscalations_ActionItems_ActionItemId",
+                        column: x => x.ActionItemId,
+                        principalTable: "ActionItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActionItemEscalations_AspNetUsers_EscalatedByUserId",
+                        column: x => x.EscalatedByUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActionItemEscalations_ActionItemId",
+                table: "ActionItemEscalations",
+                column: "ActionItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActionItemEscalations_EscalatedByUserId",
+                table: "ActionItemEscalations",
+                column: "EscalatedByUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActionItemEscalations");
+        }
+    }
+}
