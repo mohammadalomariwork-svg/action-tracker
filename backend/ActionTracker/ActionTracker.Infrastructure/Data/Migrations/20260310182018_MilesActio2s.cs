b1d0ff625b91965784fbd24a469a52a343986d6a
mohammadalomariwork-svg <Mohammad.alomari.work@gmail.com>
2026-03-10 22:34:01 +0400

    2026-03-10-After AAdding Milestones Actions

---
+++ backend/ActionTracker/ActionTracker.Infrastructure/Data/Migrations/20260310182018_MilesActio2s.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActionTracker.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MilesActio2s : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
